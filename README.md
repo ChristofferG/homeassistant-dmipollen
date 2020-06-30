8b4ca2d8babe343bfcb093b8c7d82fd81e22086a
Christoffer Graversen <christoffer.graversen@siemens.com>
2020-06-30 22:14:14 +0200

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# homeassistant-dmipollen
-Home Assistant Custom Component showing pollen data from dmi.dk
+# HomeAssistant-DMIPollen
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg)](https://github.com/custom-components/hacs)
+Home Assistant Custom Component showing pollen data from dmi.dk (Danmarks Meteorologiske Institut) by use of the REST response from: [https://www.dmi.dk/dmidk_byvejrWS/rest/texts/forecast/pollen/Danmark/](https://www.dmi.dk/dmidk_byvejrWS/rest/texts/forecast/pollen/Danmark/)
+
+## Installation
+
+### Manual Installation
+  1. Copy dmipollen folder into the custom_components folder of your home assistant configuration directory.
+  2. Restart Home Assistant.
+  3. Configure the `dmipollen` sensor in configuration.yaml as explained futher down.
+  4. Restart Home Assistant.
+
+### Installation via HACS (Home Assistant Community Store)
+  1. Ensure that [HACS](https://hacs.xyz/) is installed.
+  2. Search for and install the `dmipollen` integration.
+  3. Confiure the `eloverblik` sensor.
+  4. Restart Home Assistant.
+
+
+## Configuration
+
+`dmipollen` can only be configured via configuration.yaml
+
+```yaml
+# Example configuration.yaml entry
+
+sensor:
+  - platform: dmipollen
+    region: viborg 
+    scan_interval: 3600
+    resources:
+      - birk
+      - bynke
+      - el
+      - elm
+      - græs
+      - hassel
+      - alternaria
+      - cladosporium
+      - forecast
+      - polleninfo
+      - lastupdate
+```
+
+### CONFIGURATION PARAMETERS
+
+|Parameter |Optional|Description
+|:----------|----------|------------
+| `region` | No | Measurement spot. `københavn`, `viborg` or `all` is available. `all` will include both København and Viborg.
+|`scan_interval` | Yes | How offent the component will poll dmi.dk. The data on dmi.dk is only updated once a day, typical in the afternoon. The default value of the scan_interval is 3600sec (1hour)
+
+### CONFIGURATION RESOURCES
+
+|Resources |Optional|Description
+|:----------|----------|------------
+| `birk` | Yes | Return state of `birk`
+|`bynke` | Yes | Return state of `bynke`
+|`el` | Yes | Return state of `el`
+|`elm` | Yes | Return state of `elm`
+|`græs` | Yes | Return state of `græs`
+|`hassel` | Yes | Return state of `hassel`
+|`alternaria` | Yes | Return state of `alternaria` (Meassueremnt only available from København)
+|`cladosporium` | Yes | Return state of `cladosporium` (Meassuremnet only available from København)
+|`forecast` | Yes | Return tomorrows pollenforecast as a danish text string
+|`polleninfo` | Yes | Return information string about the pollen is meassured
+|`lastupdate` | Yes | Return the last update timestamp as a string in the format: 'tirsdag den 30. juni 2020'
+
+## States and attributes
+
+A sensor will be created with the naming syntax:
+* sensor.pollen_viborg_birk and a Friendly name: Pollen Viborg Birk
+
+Each sensor will have the attributes `last_update` timestamp and `attribution` with information about where the data comes from.
+
+## Future
+---
+As dmi.dk is working on providing free data the source of the pollen data will properly change during the next years.[https://confluence.govcloud.dk/display/FDAPI](https://confluence.govcloud.dk/display/FDAPI)
---
+++ custom_components/dmipollen/sensor.py
@@ -0,0 +1,219 @@
+"""
+Reading pollen data and forecast from DMI.dk
+
+configuration.yaml
+
+sensor:
+  - platform: dmipollen
+    region: all //all, viborg or københavn 
+    scan_interval: 30
+    resources:
+      - birk
+      - bynke
+      - el
+      - elm
+      - græs
+      - hassel
+      - alternaria
+      - cladosporium
+      - forecast
+      - polleninfo
+      - lastupdate
+"""
+
+import logging
+from datetime import datetime, timedelta
+import voluptuous as vol
+import hashlib
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (
+        CONF_REGION, CONF_RESOURCES
+    )
+from homeassistant.util import Throttle
+from homeassistant.helpers.entity import Entity
+
+from urllib.request import urlopen
+import json
+import xml.etree.ElementTree as ET
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=3600) 
+
+CONCENTRATION_PARTS_PER_CUBIC_METER = "ppcm"
+SENSOR_PREFIX = 'Pollen '
+REGIONNAME1 = 'københavn'
+REGIONNAME2 = 'viborg'
+SENSOR_TYPES = {
+    'birk': ['Birk', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'bynke': ['Bynke', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'el': ['El', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'elm': ['Elm', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'græs': ['Græs', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'hassel': ['Hassel', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'alternaria': ['Alternaria', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'cladosporium': ['Cladosporium', 'mdi:leaf', CONCENTRATION_PARTS_PER_CUBIC_METER],
+    'forecast': ['Forecast', 'mdi:chart-line', ''],
+    'polleninfo': ['Polleninfo', 'mdi:information-outline', ''],
+    'lastupdate': ['Last update','mdi:update',''],
+}
+
+ATTR_LAST_UPDATE = 'last_update'
+ATTR_ATTRIBUTION = 'attribution'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_REGION): cv.string,
+    vol.Required(CONF_RESOURCES, default=[]):
+        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
+})
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the sensor platform."""
+    region = config.get(CONF_REGION).lower()
+    try:
+        data = DMIPollenData(region)
+    except RunTimeError:
+        _LOGGER.error("Unable to connect and fetch data from dmi.dk")
+        return False
+
+    entities = []
+
+    for resource in config[CONF_RESOURCES]:
+        sensor_type = resource.lower()
+
+        if region == REGIONNAME1 or region == "all":
+            entities.append(DMIPollenSensor(data, sensor_type, REGIONNAME1))
+
+        if region == REGIONNAME2 or region == "all":     
+            entities.append(DMIPollenSensor(data, sensor_type, REGIONNAME2))    
+
+    add_entities(entities)
+
+class DMIPollenData(object):
+    """Representaition of Pollen data """
+
+    def __init__(self, region):
+        """ Initialize reading """
+        self._region = region
+        self.regionnames = []
+        self.readings = []
+        self.forecasts = []
+        self.copyrighttext = None
+        self.polleninfo = None
+        self.date_dk = None
+        self.date_string = None
+    
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        url = 'https://www.dmi.dk/dmidk_byvejrWS/rest/texts/forecast/pollen/Danmark/'
+        response = urlopen(url)
+        string = response.read().decode('utf-8')
+        json_obj = json.loads(string)
+
+        root = ET.fromstring(json_obj [0]["products"]["text"])
+
+        self.copyrighttext  = root.find('copyright').text
+        self.polleninfo = root.find('info').text
+        self.date_string = root.find('file_info').find('date_DK').find('date_string').text
+
+        year = root.find('file_info').find('date_DK').find('year').text
+        month = root.find('file_info').find('date_DK').find('month').text
+        day = root.find('file_info').find('date_DK').find('day').text
+        time = root.find('file_info').find('date_DK').find('time').text.split(':')
+        self.date_dk = datetime(int(year), int(month), int(day), int(time[0]), int(time[1]), int(time[2]))
+
+        self.regionnames.clear()
+        self.readings.clear()
+        self.forecasts.clear()
+
+        for region in root.findall('region'):
+            if region.find('name').text.lower() == self._region or self._region == 'all': 
+                self.regionnames.append( region.find('name').text )
+                self.readings.append( region.find('readings') )
+                self.forecasts.append( region.find('forecast').text)
+        
+        _LOGGER.debug("Date_dk = %s", self.date_dk)
+        _LOGGER.debug("Regionnames = %s", self.regionnames)
+        _LOGGER.debug("Readings = %s", self.readings)
+        _LOGGER.debug("Forecasts = %s", self.forecasts)
+
+class DMIPollenSensor(Entity):
+    """Representation of a Pollen sensor."""
+
+    def __init__(self, data, sensor_type, region):
+        """Initialize the sensor."""
+        self.data = data
+        self.type = sensor_type
+        self.region = region
+        self._name = SENSOR_PREFIX + self.region.title() + " " + SENSOR_TYPES[self.type][0]
+        self._unit_of_measurement = SENSOR_TYPES[self.type][2]
+        self._icon = SENSOR_TYPES[self.type][1]
+        self._attribution = None
+        self._polleninfo = None
+        self._state = None
+        self._last_update = None
+        self.update()
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+    
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return self._icon
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return self._unit_of_measurement
+
+    @property
+    def attribution(self):
+        """Return the copyright text from dmi."""
+        return self._attribution 
+
+    @property
+    def last_update(self):
+        """Return the copyright text from dmi."""
+        return self._last_update
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return{
+            ATTR_LAST_UPDATE: self._last_update,
+            ATTR_ATTRIBUTION: self._attribution,
+        }
+
+    def update(self):
+        '''Get the latest data and use it to update our sensor state. '''
+        self.data.update()
+
+        self._last_update = self.data.date_dk
+
+        self._attribution = self.data.copyrighttext
+
+        self._polleninfo = self.data.polleninfo
+
+        for index, regionname in enumerate(self.data.regionnames):
+            if regionname.lower() == self.region.lower():
+                for reading in self.data.readings[index]:
+                    if reading.find('name').text.lower() == self.type.lower():
+                        self._state = reading.find('value').text
+                        if reading.find('value').text == '-':
+                            self._state = 0
+                    elif self.type.lower() == "forecast":
+                        self._state = self.data.forecasts[index]
+                    elif self.type.lower() == "lastupdate":
+                        self._state = self.data.date_string
+                    elif self.type.lower() == "polleninfo":
+                        self._state = self._polleninfo
